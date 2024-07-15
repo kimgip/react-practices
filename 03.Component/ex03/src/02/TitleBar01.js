fdb3b59bf2b6b5d82e8117323ccf696cd8ab31ae
kimgip <gippeum0102@gmail.com>
2024-07-15 16:54:42 +0900

    Handler 예제: Function Handler - Class / Function Component

--- 03.Component/ex03/src/02/TitleBar01.js
+++ 03.Component/ex03/src/02/TitleBar01.js
@@ -5,10 +5,14 @@ export default class TitelBar01 extends Component {
         super(props);
     }
 
+    onClickHandler() {
+        console.log('TitelBar01: clicked!');
+    }
+
     render() {
         return (
             <div>
-                <h4>
+                <h4 onClick={this.onClickHandler}>
                     Function Handler in Class Component(click here!)
                 </h4>
             </div>
--- 03.Component/ex03/src/02/TitleBar02.js
+++ 03.Component/ex03/src/02/TitleBar02.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-function TitelBar02() {
-
+function TitleBar02() {
+    const onClickHandler = () => {
+        console.log('TitleBar02: clicked!')
+    }
 
 
     return (
         <div>
-            <h4>
+            <h4 onClick={onClickHandler}>
                 Function Handler in Function Component(click here!)
             </h4>
         </div>
     )
 }
 
-export default TitelBar02;
+export default TitleBar02;
