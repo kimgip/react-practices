df05c034079424decb1ed09fe1592c24acf2d7aa
kimgip <gippeum0102@gmail.com>
2024-07-15 17:09:01 +0900

    using ref 예제: 함수 / 클래스 컴포넌트

--- 03.Component/ex03/src/06/App.js
+++ 03.Component/ex03/src/06/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './assets/scss/App.scss';
+import { func } from 'prop-types';
 
 export default class App extends Component {
     constructor(props) {
@@ -8,9 +9,19 @@ export default class App extends Component {
 
     render() {
         return (
-            <div className={'App'}>
-                <div>
+            <div
+            ref={(ref) => this.outerRef = ref}
+            className={'App'}
+            onScroll={e => {
+                console.log(this.outerRef.scrollTop, this.outerRef.clientHeight, this.innerRef.clientHeight);
+            }}>
+                <div ref={(ref) => this.innerRef = ref}>
                     <ul>
+                        {
+                            Array
+                                .from({length:100}, (_, i) => i + 1)
+                                .map(e => <li key={e}>{`아이템 ${e}입니다.`}</li>)
+                        }
                     </ul>
                 </div>
             </div>
