313e8728ed19dde4a3410ae3eb568eaa548fc22c
kimgip <gippeum0102@gmail.com>
2024-07-19 18:23:52 +0900

    Hash / Browser Router lib, a tag

--- 04.Routing/src/03/component/Guestbook.js
+++ 04.Routing/src/03/component/Guestbook.js
@@ -4,6 +4,12 @@ export default function Guestbook() {
     return (
         <div>
             <h1>Guestbook</h1>
+            <ul>
+                <a href='/' onClick={(e)=>{
+                    e.preventDefault();
+                    history.pushState();
+                }}>메인</a>
+            </ul>
         </div>
     );
 }
