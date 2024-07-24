3b4511b8b2def666757081b82bf584230238c52c
kimgip <gippeum0102@gmail.com>
2024-07-24 16:11:40 +0900

    Component: lifecycle: 예제 clock: class / func

--- 03.Component/ex05/src/03/App.js
+++ 03.Component/ex05/src/03/App.js
@@ -5,18 +5,38 @@ import Clock from './Clock';
 export default class App extends Component {
     constructor(props) {
         super(props);
+        this.state = this.__getCurrentTime();
     }
     
+    __getCurrentTime() {
+        const now = new Date();
+        return {
+            hours: now.getHours(),
+            minutes: now.getMinutes(),
+            seconds: now.getSeconds(),
+            ticks: this.state ? this.state.ticks + 1 : 0
+        };
+    }
+
+    componentDidMount() {
+        this.intervalId = setInterval(() => {
+            this.setState(this.__getCurrentTime());
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+       clearInterval(this.intervalId );
+    }
+
     render() {
-        return (
-            <div className='clock-display'>
+        return ( 
+            this.state.ticks % 10 === 0 ?
+                null:
                 <Clock
-                    title={'ex05: Clock Component I'}
-                    hours={'00'}
-                    minutes={'00'}
-                    seconds={'00'}
-                    meridiem={'pm'} />
-            </div>
+                    title={`ex05: Clock Component I: ${this.state.ticks}`}
+                    hours={this.state.hours}
+                    minutes={this.state.minutes}
+                    seconds={this.state.seconds} />
         );
     }
 }
