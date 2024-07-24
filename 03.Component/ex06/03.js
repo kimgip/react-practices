7352b64a6ef18f5668545d51417506da68ad712a
kimgip <gippeum0102@gmail.com>
2024-07-24 16:11:57 +0900

    immutability

--- 03.Component/ex06/03.js
+++ 03.Component/ex06/03.js
@@ -1,7 +1,9 @@
 import fs from 'fs';
 
 console.log("== Violation ===================================");
-let state = {
-    order: JSON.parse(fs.readFileSync('./json/data.json', 'utf-8'))
-};
+let order = JSON.parse(fs.readFileSync('./json/data.json', 'utf-8'));
 
+const updateOrder = Object.assign({}, order, {receive: '강남구 논현동...'});
+updateOrder.payment.method = "mobile";
+
+console.log(order, updateOrder);
